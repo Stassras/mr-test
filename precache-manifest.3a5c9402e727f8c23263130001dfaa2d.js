4bbb1cf8f64f904ce3779973862424644fd23979
Stassras <stasbarinoff@gmailcom>
2019-11-07 21:35:27 +0300

    Updates

---
+++ precache-manifest.3a5c9402e727f8c23263130001dfaa2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "200e3c6a2fec83aa37cdc3605dd69bbd",
+    "url": "/mr-test/index.html"
+  },
+  {
+    "revision": "39536480ce540e50a6d9",
+    "url": "/mr-test/static/css/main.cf8ad289.chunk.css"
+  },
+  {
+    "revision": "7c2aa67c836f6c9891ad",
+    "url": "/mr-test/static/js/2.830f45af.chunk.js"
+  },
+  {
+    "revision": "39536480ce540e50a6d9",
+    "url": "/mr-test/static/js/main.d53c1bf9.chunk.js"
+  },
+  {
+    "revision": "4ae0a8966dba90207fa0",
+    "url": "/mr-test/static/js/runtime-main.b3546099.js"
+  }
+]);
